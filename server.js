7971e524dd86e72e3e9fbaceebce9c077d668cc3
Joey <jdechant970@gmail.com>
2019-02-10 16:13:56 -0700

    changed permissions to executable, trying again

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ require("./routes/cms-api-routes.js")(app);
 require("./routes/front-api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-// Syncing our sequelize models and then starting our Express apps
+// Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({/*force : true*/}).then(function() {
   app.listen(PORT, function() {
