cbc6e7b0260329ce7978160d9065e1e364563869
Joey <jdechant970@gmail.com>
2019-02-10 16:02:53 -0700

    testing www folder

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ require("./routes/html-routes.js")(app);
 // =============================================================
 db.sequelize.sync({/*force : true*/}).then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT " + PORT + "!!");
   });
 });
